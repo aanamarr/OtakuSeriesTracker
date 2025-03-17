492da920d933b68bce6d5dfb6f4a84bfc20aea77
Ana Maria <naoseidenada0418@gmail.com>
2025-03-17 17:34:28 +0100

    changes in main.scss

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,20 @@
 import { defineConfig } from "vite";
 import react from '@vitejs/plugin-react';
-import injectHTML from "vite-plugin-html-inject";
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
-import concat from '@vituum/vite-plugin-concat';
-import FastGlob from 'fast-glob';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
   base: "/OtakuSeriesTracker/",
-  root: "src",
-  publicDir: "../public",
+  publicDir: "public",
   build: {
     minify: "esbuild",
     outDir: "docs",
     emptyOutDir: true,
     sourcemap: true,
     rollupOptions: {
-      input: "src/index.html",
+      input: "index.html",
     },
   },
   server: {
     open: "/index.html",
   },
+  plugins: [react()], 
 });
