f33361a38ec29334a1511e925bb71ec1f29cc5c9
Ana Maria <naoseidenada0418@gmail.com>
2025-03-17 16:18:15 +0100

    changes in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,51 +7,20 @@ import FastGlob from 'fast-glob';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
-
-const htmlFilesList = Object.fromEntries(
-  FastGlob.sync('src/**/*.html').map(file => [
-    path.relative(
-      'src',
-      file.slice(0, file.length - path.extname(file).length)
-    ),
-    fileURLToPath(new URL(file, import.meta.url))
-  ])
-);
-
-const inputFilesList = {
-  ...htmlFilesList,
-  'main': 'src/js/main.js',
-};
-
 export default defineConfig({
   base: "/OtakuSeriesTracker/",
   root: "src",
-  publicDir: "public",
-
+  publicDir: "../public",
   build: {
     minify: "esbuild",
-    outDir: "../docs",
-    sourcemap: "inline",
+    outDir: "docs",
     emptyOutDir: true,
+    sourcemap: true,
     rollupOptions: {
-      input: inputFilesList,
-      output: {
-        sourcemap: true,
-        entryFileNames: ({ name }) => {
-          if (name === 'main') {
-            return 'js/main.js';
-          }
-          return "[name].js";
-        },
-      },
+      input: "src/index.html",
     },
   },
   server: {
     open: "/index.html",
-    watch: {
-      usePolling: true,
-    },
-    hmr: true,
   },
-  plugins: [react()],
 });
